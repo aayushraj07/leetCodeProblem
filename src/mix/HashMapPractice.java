4c9d8c2dd0877f060cc483d8cab22b5371cf1361
aayushraj07 <aayush.97raj@gmail.com>
2024-05-01 10:35:55 +0530

    hashmap random question

---
+++ src/mix/HashMapPractice.java
@@ -0,0 +1,29 @@
+package src.mix;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapPractice {
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,2,5,2,42};
+
+        String[] strings = {"Aayush", "Raj", "Honey", "Indian"};
+
+        HashMap<Integer, String> map = new HashMap<>();
+
+        int stringIndex = 0;
+        for(int i = 0; i< arr.length; i++){
+            if(stringIndex < strings.length){
+                map.put(arr[i], strings[stringIndex]);
+                stringIndex++;
+            }else{
+                map.put(arr[i], null);
+            }
+        }
+
+        for(Map.Entry<Integer, String> entry : map.entrySet()){
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
+    }
+}
---
+++ src/pepCoding/BinaryTree/BinaryTree.java
@@ -0,0 +1,72 @@
+package src.pepCoding.BinaryTree;
+
+import java.util.Stack;
+
+public class BinaryTree {
+
+    public static class Node{
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data, Node left, Node right){
+            this.data = data;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public static class Pair{
+        Node node;
+        int state;
+        Pair(Node node, int state){
+            this.node = node;
+            this.state = state;
+        }
+    }
+
+    public static void display(Node node){
+        if(node == null){
+            return;
+        }
+        String str = "";
+        str += node.left  == null? "." :node.left.data + " ";
+        str += " <- " + node.data + " -> ";
+        str += node.right == null ? "." :node.right.data + "";
+        System.out.println(str);
+        display(node.left);
+        display(node.right);
+    }
+    public static void main(String[] args) throws Exception{
+        Integer[] arr = {50, 25, 12 ,null, null, 37, 30, null, null, null, 75, 62, null, 70, null, null, 87, null, null};
+        Stack<Pair> st = new Stack<>();
+        Node root = new Node(arr[0], null, null);
+        Pair rtp = new Pair(root, 1);
+        st.push(rtp);
+        int index =0;
+        while(st.size() > 0){
+            Pair top =st.peek();
+            if(top.state == 1){
+                index++;
+                if(arr[index] != null){
+                    Node ln = new Node(arr[index], null,null);
+                    top.node.left = ln;
+                    Pair lp = new Pair(ln, 1);
+                    st.push(lp);
+                }
+                top.state++;
+            }else if(top.state == 2){
+                index++;
+                if(arr[index] != null) {
+                    Node rn = new Node(arr[index], null, null);
+                    top.node.right = rn;
+                    Pair rp = new Pair(rn, 1);
+                    st.push(rp);
+                }
+                top.state++;
+            }else{
+                st.pop();
+            }
+        }
+        display(root);
+    }
+}
